54dda1cddccb051319907c5572b769112f13f006
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-08-25 16:57:06 -0700

    not finished with key press and click counter

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
-// import functions
+//cat 
 
-// reference needed DOM elements
+const catSound = document.getElementById('cat');
+const catPicture = document.getElementById('catpic');
 
 
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+
+catPicture.addEventListener('click', () => {
+    catSound.play();
+    catPicture.classList.add('selected');
+    dogPicture.classList.remove('selected');
+    horsePicture.classList.remove('selected');
+
+});
+
+//dog
+const dogSound = document.getElementById('dog');
+const dogPicture = document.getElementById('dogpic');
+
+
+
+dogPicture.addEventListener('click', () => {
+    dogSound.play();
+    dogPicture.classList.add('selected');
+    catPicture.classList.remove('selected');
+    horsePicture.classList.remove('selected');
+});
+
+//horse 
+const horseSound = document.getElementById('horse');
+const horsePicture = document.getElementById('horsepic');
+
+
+
+horsePicture.addEventListener('click', () => {
+    horseSound.play();
+    horsePicture.classList.add('selected');
+    catPicture.classList.remove('selected');
+    dogPicture.classList.remove('selected');
+});
+
+//key pres
+document.addEventListener('keydown', event => {
+    if(event.key === 'c') {
+        catSound.play();
+        
+    
+    }
+});
